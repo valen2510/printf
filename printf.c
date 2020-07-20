12321bfafcf2608ece754086cc19ab780dc0b4f5
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-20 18:12:13 -0500

    Add chaeck process comparison for specifier %c

--- printf.c
+++ printf.c
@@ -7,9 +7,35 @@
 int _printf(const char *format, ...)
 {
 	types printf[] = {
-		{"c", printc}
+		{"c", printc},
 		{NULL, NULL}
 	};
 
+	unsigned int i, j;
+	va_list list;
 
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; printf[j].p != NULL; j++)
+			{
+				if (format[i + 1] == printf[j].p[0])
+				{
+					printf[j].func(list);
+					i++;
+				}
+			}
+		}
+
+		else
+		{
+			_putchar(format[i]);
+		}
+
+	}
+	va_end(list);
+	return (0);
 }
