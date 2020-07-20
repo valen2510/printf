44b7e10adb69b502485776fd01ebe63596729210
camagar <mauriciogrestrepo@gmail.com>
2020-07-20 19:04:24 +0000

    Holberton_h

--- holberton.h
+++ holberton.h
@@ -1,6 +1,12 @@
 #ifndef Holberton_h
 #define Holberton_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 
 #endif /* Holberton_h */
