2a02f2a08058856cdf579be2812705053870e621
camagar <mauriciogrestrepo@gmail.com>
2020-07-20 23:26:46 +0000

    printf_functions

--- printf_functions.c
+++ printf_functions.c
@@ -1,16 +1,22 @@
 #include "holberton.h"
 /**
- *
+ * _putchar - Entry function
+ * @c: variable
+ * return: print.
  */
 int _putchar(char c)
 {
 	return(write(1, &c, 1));
 }
 /**
+ * printc - Entry function
+ * @list: variable
  *
+ * return: integer.
  */
-void printc(va_list list)
+int printc(va_list list)
 {
 	_putchar(va_arg(list, int));
+	return (1);
 }
 
