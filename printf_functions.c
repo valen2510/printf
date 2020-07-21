164ca7f70a0adcd958fc98dea212883f938d5a37
camagar <mauriciogrestrepo@gmail.com>
2020-07-21 01:32:49 +0000

    Aux functions

--- printf_functions.c
+++ printf_functions.c
@@ -2,25 +2,44 @@
 /**
  * _putchar - Entry function
  * @c: variable
- * return: print.
+ *
+ * Return: 0.
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 /**
  * printc - Entry function
  * @list: variable
  *
- * return: integer.
+ * Return: 0.
  */
 int printc(va_list list)
 {
 	_putchar(va_arg(list, int));
-<<<<<<< HEAD
 	return (1);
-=======
-	return (0);
->>>>>>> 12321bfafcf2608ece754086cc19ab780dc0b4f5
-}
 
+}
+/**
+ * print_string - Entry point.
+ * @s: variable
+ *
+ * Return: 0
+ */
+int print_string(va_list s)
+{
+	char *str;
+	int k;
+
+	str = va_arg(s, char*);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		_putchar(str[k]);
+	}
+	return (k);
+}
