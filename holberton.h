0363f3865e1662129aae94c4dc6efc63e343dd62
camagar <mauriciogrestrepo@gmail.com>
2020-07-21 01:33:16 +0000

    Holberton header file

--- holberton.h
+++ holberton.h
@@ -11,6 +11,7 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int printc(va_list l);
 int _putchar(char c);
+int print_string(va_list s);
 
 /**
  * struct types - specifier structure for printf 
